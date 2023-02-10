59d34007935e874dba9ef361a728048ff2a6bdb7
Pedro Carvalho <61274843+PedroCarvalho98@users.noreply.github.com>
2023-02-10 16:03:20 +0000

    Delete YOLOv8_live_testing.py

--- YOLOv8_live_testing.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-import cv2
-
-# Download the yolo model previously
-model = YOLO("yolov8x.pt") # path to the model
-results = model.predict(source="0", show=True, conf=0.7) # source = 0 specifies the source of image to predict (webcam)
-
-print(results)
